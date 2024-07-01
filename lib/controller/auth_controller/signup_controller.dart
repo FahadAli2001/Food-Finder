231deaf479161f08f2bb609083dd6b60cece3e0d
FAHAD ALI <m.mohsin00933@gmail.com>
2024-07-01 12:23:27 -0700

    fahad

--- lib/controller/auth_controller/signup_controller.dart
+++ lib/controller/auth_controller/signup_controller.dart
@@ -127,13 +127,13 @@ class SignupController extends ChangeNotifier {
     } catch (e) {
       log(e.toString());
       isSigningUp = false;
-      // Fluttertoast.showToast(
-      //     msg: e.toString(),
-      //     toastLength: Toast.LENGTH_SHORT,
-      //     gravity: ToastGravity.CENTER,
-      //     backgroundColor: Colors.red,
-      //     textColor: Colors.white,
-      //     fontSize: 16.0);
+      Fluttertoast.showToast(
+          msg: e.toString(),
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
     }
     notifyListeners();
   }
